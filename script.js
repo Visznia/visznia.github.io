2862eabc951d5e5a4436ac11551968568e561b2f
Visznia <v1s2n1a@gmail.com>
2025-02-09 21:25:18 +0100

    update

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 function generateLabel() {
-    const gtin = document.getElementById('gtin').value;
-    const sscc = document.getElementById('sscc').value;
-    const quantity = document.getElementById('quantity').value;
+    var gtin = document.getElementById('gtin').value;
+    var sscc = document.getElementById('sscc').value;
+    var quantity = document.getElementById('quantity').value;
     const labelDiv = document.createElement('table');
     labelDiv.classList.add('label');
+
+    if(gtin){
+        gtin = `(02)${gtin}`
+    }
+    
+    if(sscc){
+        sscc = `(00)${sscc}`
+    }
+
+    if(quantity){
+        quantity = `(37)${quantity}`
+    }
     
     labelDiv.innerHTML = `
         <tbody>
         <tr>
             <td><p>GTIN<p>${gtin}</p></td>
             <td><p>SSCC<p>${sscc}</p></td>
-            <td>test;</td>
+            <td><p>QUANTITY<p>${quantity}</td>
         </tr>
         <tr>
-            <td colspan="3"><p class="barcode">${sscc}${gtin}</p><p class="barcode-text">${sscc}${gtin}</p></td>
+            <td colspan="3"><p class="barcode">${gtin}</p><p class="barcode-text">${gtin}</p></td>
         </tr>
         <tr>
-            <td colspan="3" class="barcode">test</td>
+            <td colspan="3"><p class="barcode">${quantity}</p><p class="barcode-text">${quantity}</p></td>
         </tr>
         <tr>
-            <td colspan="3" class="barcode">test</td>
+            <td colspan="3"><p class="barcode">${sscc}</p><p class="barcode-text">${sscc}</p></td>
         </tr>
         </tbody>
     `;
