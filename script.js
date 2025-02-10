86d7cd5911e2ce0b6e439cf402de8f254cc5dde1
Visznia <vv1s2n1a@gmail.com>
2025-02-10 08:18:02 +0100

    update

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
 function generateLabel() {
+    var companyname = document.getElementById('company').value;
     var gtin = document.getElementById('gtin').value;
     var sscc = document.getElementById('sscc').value;
     var quantity = document.getElementById('quantity').value;
     var batch = document.getElementById('batch').value;
     const labelDiv = document.createElement('div');
-    labelDiv.classList.add('div-round-shadow')
+    labelDiv.classList.add('div-round-shadow');
     const labelTable = document.createElement('table');
     labelTable.classList.add('label');
 
-    if(gtin){
-        gtin = `(02)${gtin}`
+    var tableText = `<tbody>`;
+
+    if(companyname){
+        tableText = tableText + `
+        <tr>
+            <td class="company-name" colspan="3"><p>${companyname}</p></td>
+        </tr>
+        `;
     }
-    
-    if(sscc){
-        sscc = `(00)${sscc}`
+
+    tableText = tableText + `<tr>`;
+
+    if(gtin){
+        gtin = `(02)${gtin}`;
+        tableText = tableText + `    <td colspan="2"><p>GTIN</p><p>${gtin}</p></td>`;
     }
 
     if(quantity){
-        quantity = `(37)${quantity}`
+        quantity = `(37)${quantity}`;
+        tableText = tableText + `    <td><p>QUANTITY</p><p>${quantity}</td>`;
+    }
+
+    tableText = tableText + `</tr>`;
+    tableText = tableText + `<tr>`;
+    
+    if(sscc){
+        sscc = `(00)${sscc}`;
+        tableText = tableText + `    <td colspan="2"><p>SSCC</p><p>${sscc}</p></td>`;
     }
 
     if(batch){
-        batch = `(10)${batch}`
+        batch = `(10)${batch}`;
+        tableText = tableText + `    <td><p>BATCH</p><p>${batch}</p></td>`;
     }
+
+    tableText = tableText + `</tr>`;
+
     
-    labelTable.innerHTML = `
-        <tbody>
-        <tr>
-            <td><p colspan="2">GTIN</p><p>${gtin}</p></td>
-            <td><p>QUANTITY</p><p>${quantity}</td>
-        </tr>
-        <tr>
-            <td><p colspan="2">SSCC</p><p>${sscc}</p></td>
-            <td><p>BATCH</p><p>${batch}</p></td>
-        </tr>
+    tableText = tableText + `
+            
         <tr>
             <td class="barcode-cell" colspan="3"><p class="barcode">${gtin}</p><p class="barcode-text">${gtin}</p></td>
         </tr>
@@ -45,6 +60,10 @@ function generateLabel() {
         </tr>
         </tbody>
     `;
+
+    labelTable.innerHTML = tableText;
+
+    console.log(labelTable.innerHTML)
     
     labelDiv.appendChild(labelTable);
     document.getElementById('labels').appendChild(labelDiv);
