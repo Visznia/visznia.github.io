56ff23d8145e998f21719cfb73737c1dccc9b6bf
Visznia <vv1s2n1a@gmail.com>
2025-10-16 20:07:45 +0200

    ŻEGLUGA ŚRÓDLĄDOWA

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
  const questions = [
       {
+        category: "Żegluga śródlądowa",
         question: "Jaka jest typowa wielkość barki rzecznej w Polsce",
         answers: [
           "300 ton",
@@ -10,6 +11,7 @@
 
       },
       {
+        category: "Żegluga śródlądowa",
         question: "W Europie największy port rzeczny jako centrum logistyczne znajduje się w",
         answers: [
                     
@@ -20,6 +22,7 @@
         correct: "Duisburgu"
       },
       {
+        category: "Żegluga śródlądowa",
         question:"W Polsce udział transportu wodnego śródlądowego w obsłudze potrzeb przewozowych waha się w granicach",
         answers:[
         "1%",
@@ -28,12 +31,223 @@
         correct: "1%"
       },
       {
+        category: "Żegluga śródlądowa",
         question:"Największym armatorem żeglugi śródlądowej w Polsce jest",
         answers:[
         "żegluga Bydgoska SA",
         "P.P. Żegluga Szczecińska",
         "Odratrans"],
         correct: "Odratrans"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "W strukturze przewozów w transporcie wodnym śródlądowym w Polsce dominują:",
+        answers: [
+          "Piasek, żwir i kamienie",
+          "Węgiel i rudy metali",
+          "Ropa naftowa i jej przetwory"
+        ],
+        correct: "Piasek, żwir i kamienie"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "W strukturze rodzajowej floty żeglugi śródlądowej w Polsce dominuje:",
+        answers: [
+          "Flota z własnym napędem",
+          "Flota pchana",
+          "Flota holowana"
+        ],
+        correct: "Flota pchana"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Jakie są obecne tendencje w Polsce dotyczące portów rzecznych?",
+        answers: [
+          "Zwiększanie liczby i obrotów portów rzecznych",
+          "Zmniejszanie się liczby i obrotów portów rzecznych",
+          "Brak zmian w liczbie i obrotach portów rzecznych"
+        ],
+        correct: "Zmniejszanie się liczby i obrotów portów rzecznych"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Przewozy krajowe w Polsce uregulowane są w:",
+        answers: [
+          "Ustawie Prawo przewozowe",
+          "Kodeksie Morskim",
+          "Umowach międzynarodowych"
+        ],
+        correct: "Ustawie Prawo przewozowe"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Przewozy międzynarodowe w Polsce uregulowane są w:",
+        answers: [
+          "Ustawie Prawo przewozowe",
+          "Kodeksie Morskim i umowach międzynarodowych",
+          "Rozporządzeniu Ministra Transportu"
+        ],
+        correct: "Kodeksie Morskim i umowach międzynarodowych"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Wymogi w zakresie kwalifikacji załogi w żegludze śródlądowej obejmują:",
+        answers: [
+          "Patent Kanałowy, Reński, Łabski",
+          "Tylko Patent Kanałowy",
+          "Patent Morski"
+        ],
+        correct: "Patent Kanałowy, Reński, Łabski"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Standardowa wielkość barki rzecznej w Europie Zachodniej wynosi:",
+        answers: [
+          "500 ton",
+          "1000 ton",
+          "1500 ton"
+        ],
+        correct: "1500 ton"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Udział przewozów międzynarodowych w żegludze śródlądowej w Polsce wynosi obecnie około:",
+        answers: [
+          "30%",
+          "50%",
+          "70%"
+        ],
+        correct: "70%"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Udział przewozów kontenerów w żegludze śródlądowej w Polsce wynosi:",
+        answers: [
+          "0%",
+          "5%",
+          "10%"
+        ],
+        correct: "0%"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Całkowita długość śródlądowych dróg wodnych w Polsce wynosi:",
+        answers: [
+          "320 km",
+          "3767,8 km",
+          "5000 km"
+        ],
+        correct: "3767,8 km"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Tylko ile procent dróg wodnych w Polsce ma znaczenie międzynarodowe?",
+        answers: [
+          "5,5%",
+          "20%",
+          "50%"
+        ],
+        correct: "5,5%"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Największa inwestycja wodna w Polsce to:",
+        answers: [
+          "Budowa stopnia wodnego w Malczycach",
+          "Budowa portu w Szczecinie",
+          "Modernizacja Kanału Gliwickiego"
+        ],
+        correct: "Budowa stopnia wodnego w Malczycach"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "System informatyczny RIS w Polsce działa na:",
+        answers: [
+          "Górnej Odrze",
+          "Dolnej Odrze",
+          "Wiśle"
+        ],
+        correct: "Dolnej Odrze"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Dostęp do rynku żeglugowego w Polsce mają:",
+        answers: [
+          "Tylko osoby prawne",
+          "Osoby prawne i fizyczne oraz nie mające osobowości prawnej",
+          "Osoby legitymujące się zaświadczeniem o posiadaniu kompetencji zawodowych"
+        ],
+        correct: "Osoby prawne i fizyczne oraz nie mające osobowości prawnej"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Wymóg zdolności zawodowej nie obowiązuje w przypadku:",
+        answers: [
+          "Dróg wodnych nieżeglownych, przewozów na potrzeby własne, statków do 200 ton i promów",
+          "Transportu międzynarodowego",
+          "Transportu kontenerowego"
+        ],
+        correct: "Dróg wodnych nieżeglownych, przewozów na potrzeby własne, statków do 200 ton i promów"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Najważniejszym dokumentem przewozowym w żegludze śródlądowej jest:",
+        answers: [
+          "Konosament",
+          "Dokument załadunkowy",
+          "Raport pomiaru barki"
+        ],
+        correct: "Konosament"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Przewozy ładunków niebezpiecznych w Polsce od 2010 r. regulowane są przez przepisy:",
+        answers: [
+          "ADN, tylko w przewozach międzynarodowych",
+          "Ustawę Prawo Przewozowe",
+          "Kodeks Morski"
+        ],
+        correct: "ADN, tylko w przewozach międzynarodowych"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Ustawa Prawo Przewozowe ma zastosowanie w transporcie wodnym śródlądowym do przewozów:",
+        answers: [
+          "Krajowych",
+          "Międzynarodowych",
+          "Krajowych i międzynarodowych"
+        ],
+        correct: "Krajowych i międzynarodowych"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Podstawowym dokumentem technicznym warunkującym dopuszczenie statku do żeglugi jest:",
+        answers: [
+          "Karta rejestracyjna statku",
+          "Świadectwo zdolności żeglugowej",
+          "Świadectwo klasyfikacyjne PRS"
+        ],
+        correct: "Świadectwo zdolności żeglugowej"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Udział przewozów międzynarodowych w przewozach ogółem żeglugi śródlądowej w Polsce wynosi aktualnie około:",
+        answers: [
+          "50%",
+          "70%",
+          "80%"
+        ],
+        correct: "80%"
+      },
+      {
+        category: "Żegluga śródlądowa",
+        question: "Bezpośredni nadzór administracyjny nad śródlądowymi drogami wodnymi w Polsce należy do:",
+        answers: [
+          "Ministerstwa Infrastruktury",
+          "Urzędów Żeglugi Śródlądowej",
+          "Regionalnych Zarządów Gospodarki Wodnej"
+        ],
+        correct: "Regionalnych Zarządów Gospodarki Wodnej"
       }
     ];
 
